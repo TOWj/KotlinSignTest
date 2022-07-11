381bb3cfaec04081309b8cb3e602598bc014d88e
Yuriy Novikov <tow1987@yandex.ru>
2022-07-11 18:48:16 +0300

    Change all

--- app/src/main/java/com/example/learnapp/Constance.kt
+++ app/src/main/java/com/example/learnapp/Constance.kt
@@ -2,16 +2,17 @@ package com.example.learnapp
 
 object Constance {
 
-    const val DIR_SALARY = "100000"
-    const val DVOR_SALARY = "1000"
-    const val INJ_SALARY = "10000"
+    const val SIGN_IN_STATE = "sign_in"
+    const val SIGN_UP_STATE = "sign_up"
+    const val SIGN_STATE = "sign_state"
+    const val REQUEST_CODE_SIGN_IN = 1212
+    const val REQUEST_CODE_SIGN_UP = 2121
 
-    const val INJ_NAME = "Inj"
-    const val DVOR_NAME = "Dvor"
-    const val DIR_NAME = "Dir"
-
-    const val INJ_CODE = "123"
-    const val DVOR_CODE = "456"
-    const val DIR_CODE = "789"
+    const val LOGIN = "login"
+    const val PASSWORD = "password"
+    const val NAME = "name"
+    const val NAME2 = "name2"
+    const val NAME3 = "name3"
+    const val AVATAR_ID = "avatar_id"
 
 }
--- app/src/main/java/com/example/learnapp/MainActivity.kt
+++ app/src/main/java/com/example/learnapp/MainActivity.kt
@@ -1,108 +1,99 @@
 package com.example.learnapp
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
+import com.example.learnapp.Constance
+import com.example.learnapp.R
+import com.example.learnapp.SignInUpAct
 import com.example.learnapp.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
-
-
     lateinit var bindingClass: ActivityMainBinding
+    private var login: String = "empty"
+    private var password: String = "empty"
+    private var name: String = "empty"
+    private var name2: String = "empty"
+    private var name3: String = "empty"
+    private var avatarImageId: Int = 0
 
-    private lateinit var launcherSignIn: ActivityResultLauncher<Intent>
-    private lateinit var launcherSignUp: ActivityResultLauncher<Intent>
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        launcherSignIn =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultIn: ActivityResult ->
-                if (resultIn.resultCode == Activity.RESULT_OK) {
-                    //If RESULT IS OK - entering
-                    //Given fields Login & Password, and button "Sign In"
-                    //If l & p is correct, transition to MainActivity
-                    //Show fields and avatar and button "Exit"
-                    bindingClass.imageView.setImageResource(R.drawable.one)
-                    bindingClass.imageView.visibility = View.VISIBLE
-                    bindingClass.signInBt.text = "Exit"
-                    bindingClass.singUpBt.visibility = View.INVISIBLE
-                    bindingClass.nameMainTv.text = resultIn.data?.getStringExtra("name").toString()
-                    bindingClass.nameMainTv.visibility = View.VISIBLE
-                    bindingClass.surnameMainTv.text = resultIn.data?.getStringExtra("surname").toString()
-                    bindingClass.surnameMainTv.visibility = View.VISIBLE
-                    bindingClass.ptrNameMainTv.text = resultIn.data?.getStringExtra("patron").toString()
-                    bindingClass.ptrNameMainTv.visibility = View.VISIBLE
-                } else {
-                    //If incorrect, also transition to MainActivity
-                    //Show text "Incorrect l & p" and both buttons "Sign"
-                    bindingClass.incorrectTv.visibility = View.VISIBLE
-                }
-            }
-        launcherSignUp =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultUp: ActivityResult ->
-                if (resultUp.resultCode == Activity.RESULT_OK) {
-                    bindingClass.imageView.setImageResource(R.drawable.one)
-                    bindingClass.imageView.visibility = View.VISIBLE
-                    bindingClass.signInBt.text = "Exit"
-                    bindingClass.singUpBt.visibility = View.INVISIBLE
-                    bindingClass.nameMainTv.text = resultUp.data?.getStringExtra("name").toString()
-                    bindingClass.nameMainTv.visibility = View.VISIBLE
-                    bindingClass.surnameMainTv.text = resultUp.data?.getStringExtra("surname").toString()
-                    bindingClass.surnameMainTv.visibility = View.VISIBLE
-                    bindingClass.ptrNameMainTv.text = resultUp.data?.getStringExtra("patron").toString()
-                    bindingClass.ptrNameMainTv.visibility = View.VISIBLE
-                    //If RESULT OK - registry
-                    //Show all fields in RegistrationActivity
-                    //Save changes is these fields
-                    //Show button "Sign Up"
-                    //After this, transition to MainActivity
-                    //Show fields and avatar and button "Exit"
-                }else{
-                    bindingClass.incorrectTv.visibility = View.VISIBLE
-                }
-            }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-    fun onClickSignIn(view: View) {
-        if (bindingClass.signInBt.text.toString() == "Exit") {
-            resultBadAct()
-        } else {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            intent.putExtra("keyword", "signIn")
-            launcherSignIn.launch(intent)
+        if(requestCode == Constance.REQUEST_CODE_SIGN_IN){
+            val l = data?.getStringExtra(Constance.LOGIN)
+            val p = data?.getStringExtra(Constance.PASSWORD)
+            if(login == l && password == p){
+
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(avatarImageId)
+                val textInfo = "$name $name2 $name3"
+                bindingClass.tvInfo.text = textInfo
+                bindingClass.bHide.visibility = View.GONE
+                bindingClass.bExit.text = "Выйти"
+
+            } else {
+
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(R.drawable.figa)
+                bindingClass.tvInfo.text = "Такого аккаунта не существует!"
+
+            }
+
+        } else if(requestCode == Constance.REQUEST_CODE_SIGN_UP){
+
+            login = data?.getStringExtra(Constance.LOGIN)!!
+            password = data.getStringExtra(Constance.PASSWORD)!!
+            name = data.getStringExtra(Constance.NAME)!!
+            name2 = data.getStringExtra(Constance.NAME2)!!
+            name3 = data.getStringExtra(Constance.NAME3)!!
+            avatarImageId = data.getIntExtra(Constance.AVATAR_ID, 0)
+            bindingClass.imAvatar.visibility = View.VISIBLE
+            bindingClass.imAvatar.setImageResource(avatarImageId)
+            val textInfo = "$name $name2 $name3"
+            bindingClass.tvInfo.text = textInfo
+            bindingClass.bHide.visibility = View.GONE
+            bindingClass.bExit.text = "Выйти"
+
         }
     }
 
-    fun onClickSignUp(view: View) {
-        if (bindingClass.signInBt.text.toString() == "Exit") {
-            resultBadAct()
+    fun onClickSignIn(view: View){
+
+        if(bindingClass.imAvatar.isVisible && bindingClass.tvInfo.text.toString() != "Такого аккаунта не существует!"){
+
+            bindingClass.imAvatar.visibility = View.INVISIBLE
+            bindingClass.tvInfo.text = ""
+            bindingClass.bHide.visibility = View.VISIBLE
+            bindingClass.bExit.text = getString(R.string.sign_in)
+
         } else {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            intent.putExtra("keyword", "signUp")
-            launcherSignUp.launch(intent)
+
+            val intent = Intent(this, SignInUpAct::class.java)
+            intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_IN_STATE)
+            startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_IN)
+
         }
+
     }
 
-    private fun resultBadAct(){
-        val intent = Intent(this, RegistrationActivity::class.java)
-        bindingClass.imageView.visibility = View.INVISIBLE
-        bindingClass.signInBt.text = getString(R.string.sign_in)
-        bindingClass.singUpBt.visibility = View.VISIBLE
-        bindingClass.nameMainTv.visibility = View.INVISIBLE
-        bindingClass.surnameMainTv.visibility = View.INVISIBLE
-        bindingClass.ptrNameMainTv.visibility = View.INVISIBLE
+    fun onClickSignUp(view: View){
+
+        val intent = Intent(this, SignInUpAct::class.java)
+        intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_UP_STATE)
+        startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_UP)
+
     }
+
+
 }
