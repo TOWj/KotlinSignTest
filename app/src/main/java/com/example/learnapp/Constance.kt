05042b812107807d67ab5b7fefb33927d4a30195
Yuriy Novikov <tow1987@yandex.ru>
2022-07-11 12:36:46 +0300

    first commit

---
+++ app/src/main/java/com/example/learnapp/Constance.kt
@@ -0,0 +1,17 @@
+package com.example.learnapp
+
+object Constance {
+
+    const val DIR_SALARY = "100000"
+    const val DVOR_SALARY = "1000"
+    const val INJ_SALARY = "10000"
+
+    const val INJ_NAME = "Inj"
+    const val DVOR_NAME = "Dvor"
+    const val DIR_NAME = "Dir"
+
+    const val INJ_CODE = "123"
+    const val DVOR_CODE = "456"
+    const val DIR_CODE = "789"
+
+}
---
+++ app/src/main/java/com/example/learnapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.learnapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.learnapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var bindingClass: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+
+    }
+}
