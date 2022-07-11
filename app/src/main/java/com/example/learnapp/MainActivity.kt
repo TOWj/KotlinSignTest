879d95baf6aee8a600e78570511dccb8f95d88d1
Yuriy Novikov <tow1987@yandex.ru>
2022-07-11 16:04:08 +0300

    Few changes

--- app/src/main/java/com/example/learnapp/MainActivity.kt
+++ app/src/main/java/com/example/learnapp/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.learnapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.learnapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
     lateinit var bindingClass: ActivityMainBinding
+
+    private lateinit var launcherSignIn: ActivityResultLauncher<Intent>
+    private lateinit var launcherSignUp: ActivityResultLauncher<Intent>
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +27,82 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
+        launcherSignIn =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultIn: ActivityResult ->
+                if (resultIn.resultCode == Activity.RESULT_OK) {
+                    //If RESULT IS OK - entering
+                    //Given fields Login & Password, and button "Sign In"
+                    //If l & p is correct, transition to MainActivity
+                    //Show fields and avatar and button "Exit"
+                    bindingClass.imageView.setImageResource(R.drawable.one)
+                    bindingClass.imageView.visibility = View.VISIBLE
+                    bindingClass.signInBt.text = "Exit"
+                    bindingClass.singUpBt.visibility = View.INVISIBLE
+                    bindingClass.nameMainTv.text = resultIn.data?.getStringExtra("name").toString()
+                    bindingClass.nameMainTv.visibility = View.VISIBLE
+                    bindingClass.surnameMainTv.text = resultIn.data?.getStringExtra("surname").toString()
+                    bindingClass.surnameMainTv.visibility = View.VISIBLE
+                    bindingClass.ptrNameMainTv.text = resultIn.data?.getStringExtra("patron").toString()
+                    bindingClass.ptrNameMainTv.visibility = View.VISIBLE
+                } else {
+                    //If incorrect, also transition to MainActivity
+                    //Show text "Incorrect l & p" and both buttons "Sign"
+                    bindingClass.incorrectTv.visibility = View.VISIBLE
+                }
+            }
+        launcherSignUp =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultUp: ActivityResult ->
+                if (resultUp.resultCode == Activity.RESULT_OK) {
+                    bindingClass.imageView.setImageResource(R.drawable.one)
+                    bindingClass.imageView.visibility = View.VISIBLE
+                    bindingClass.signInBt.text = "Exit"
+                    bindingClass.singUpBt.visibility = View.INVISIBLE
+                    bindingClass.nameMainTv.text = resultUp.data?.getStringExtra("name").toString()
+                    bindingClass.nameMainTv.visibility = View.VISIBLE
+                    bindingClass.surnameMainTv.text = resultUp.data?.getStringExtra("surname").toString()
+                    bindingClass.surnameMainTv.visibility = View.VISIBLE
+                    bindingClass.ptrNameMainTv.text = resultUp.data?.getStringExtra("patron").toString()
+                    bindingClass.ptrNameMainTv.visibility = View.VISIBLE
+                    //If RESULT OK - registry
+                    //Show all fields in RegistrationActivity
+                    //Save changes is these fields
+                    //Show button "Sign Up"
+                    //After this, transition to MainActivity
+                    //Show fields and avatar and button "Exit"
+                }else{
+                    bindingClass.incorrectTv.visibility = View.VISIBLE
+                }
+            }
+    }
 
+
+    fun onClickSignIn(view: View) {
+        if (bindingClass.signInBt.text.toString() == "Exit") {
+            resultBadAct()
+        } else {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            intent.putExtra("keyword", "signIn")
+            launcherSignIn.launch(intent)
+        }
+    }
+
+    fun onClickSignUp(view: View) {
+        if (bindingClass.signInBt.text.toString() == "Exit") {
+            resultBadAct()
+        } else {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            intent.putExtra("keyword", "signUp")
+            launcherSignUp.launch(intent)
+        }
+    }
+
+    private fun resultBadAct(){
+        val intent = Intent(this, RegistrationActivity::class.java)
+        bindingClass.imageView.visibility = View.INVISIBLE
+        bindingClass.signInBt.text = getString(R.string.sign_in)
+        bindingClass.singUpBt.visibility = View.VISIBLE
+        bindingClass.nameMainTv.visibility = View.INVISIBLE
+        bindingClass.surnameMainTv.visibility = View.INVISIBLE
+        bindingClass.ptrNameMainTv.visibility = View.INVISIBLE
     }
 }
--- app/src/main/java/com/example/learnapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/learnapp/RegistrationActivity.kt
@@ -1,11 +1,76 @@
 package com.example.learnapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.learnapp.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var message: String
+
+
+    private var name: String = "default"
+    private var surname: String = "default"
+    private var patron: String = "default"
+    private lateinit var login: String
+    private lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        message = intent.getStringExtra("keyword").toString()
+
+        if (message == "signIn") {
+            //if entering
+            binding.registrationBt.text = getString(R.string.sign_in)
+            binding.nameEt.visibility = View.INVISIBLE
+            binding.surnameEt.visibility = View.INVISIBLE
+            binding.patronicNameEt.visibility = View.INVISIBLE
+        } else if (message == "signUp") {
+            //if registry
+            binding.registrationBt.text = getString(R.string.sign_up)
+        }
+
+    }
+
+    fun onClickButton(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        if (message == "signIn") {
+            //if entering
+
+            if (login == binding.loginEt.text.toString() && password == binding.passwordEt.text.toString()) {
+                intent.putExtra("name", name)
+                intent.putExtra("surname", surname)
+                intent.putExtra("patron", patron)
+                setResult(RESULT_OK, intent)
+            } else {
+                setResult(RESULT_CANCELED, intent)
+            }
+        } else if (message == "signUp") {
+            //if registry
+            name = binding.nameEt.text.toString()
+            surname = binding.surnameEt.text.toString()
+            patron = binding.patronicNameEt.text.toString()
+            login = binding.loginEt.text.toString()
+            password = binding.passwordEt.text.toString()
+            setResult(RESULT_OK, intent)
+            intent.putExtra("name", name)
+            intent.putExtra("surname", surname)
+            intent.putExtra("patron", patron)
+            intent.putExtra("login", login)
+            intent.putExtra("password", password)
+            finish()
+        } else {
+            setResult(Activity.RESULT_CANCELED, intent)
+            finish()
+        }
+        finish()
     }
 }
